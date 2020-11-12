94072405bfd47c0d8f9e813046d36b873786c47f
AngelaKostadinova <angelakostadinova7@gmail.com>
2020-11-12 17:19:23 +0100

    initial commit

---
+++ SEDC.PizzaApp.Domain/Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SEDC.PizzaApp.Domain.Models
+{
+    public class Feedback
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FeedbackMessage { get; set; }
+        public double Rating { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
